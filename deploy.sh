a294a001b863e96e1034e3f705538abe8b204ea1
xingwu <wangyu19930107@qq.com>
2017-04-08 18:03:39 +0800

    修改框架

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,5 @@
-mvn clean package -Dmaven.test.skip -Prelease
-scp -r target/root.war root@123.206.231.180:/opt/reawei/webapps/
+cp -R ../reawei ../root
+zip -r ../root.zip ../root
+scp -r ../root.zip root@123.206.231.180:/opt/reawei/jetty/webapps/
+rm -rf ../root
+rm ../root.zip
--- src/Test/java/test/Test.java
+++
@@ -1,7 +0,0 @@
-package test;
-
-/**
- * Created by xingwu on 2017/3/28.
- */
-public class Test {
-}
